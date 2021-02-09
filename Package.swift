5fbef46bbc2a0609d69d4f840d6eb099a6175d34
sakiyamaK <sakiyama.k@gmail.com>
2021-02-09 11:24:44 +0900

    fix products

--- Package.swift
+++ Package.swift
@@ -5,6 +5,15 @@ import PackageDescription
 
 let package = Package(
     name: "QiitaCLI",
+    products: [
+        // Products define the executables and libraries produced by a package, and make them visible to other packages.
+        .library(
+            name: "QiitaCLICore",
+            targets: ["QiitaCLICore"]),
+        .executable(
+            name: "QiitaCLI",
+            targets: ["QiitaCLI"]),
+    ],
     dependencies: [
         // Dependencies declare other packages that this package depends on.
         // .package(url: /* package url */, from: "1.0.0"),
